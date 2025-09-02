bd988f94b20f265d97ce233dd11402925fd9eb55
Deepak Kumar <dipanshugothwal176@gmail.com>
2025-09-02 20:25:00 +0530

    Design Navbar and add Backend

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1> mai sign in page hai</h1>
+    <div className='mt-40'>
+      hello
     </div>
   )
 }
-export default page
 
+export default page
--- components/header.jsx
+++ components/header.jsx
@@ -1,19 +1,70 @@
 import React from 'react'
 import { SignedOut, SignedIn, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { LayoutDashboard, Link, PenBoxIcon, PenLineIcon } from 'lucide-react'
+import { Button } from './ui/button'
 const header = () => {
   return (
-    <div>
-     <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+
+      <nav className='container mx-auto px-4 flex items-centre justify-between'>
+
+        {/* logo */}
+        <a href='/'>
+          <img
+            src="logo.png"
+            alt="SpendSense logo"
+            height={120}
+            width={320}
+            className="h-20 w-auto object-contain"
+          />
+        </a>
+        {/* buttons for nav bar */}
+        <div className='flex  items-center space-x-2.5'>
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/dashboard'>
+              <Button variant="outline" className="mt-6">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            {/* button after signed up  */}
+
+            {/* for dashboard */}
+            <a
+              href={"/dashboard"}
+              className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+              <Button variant="outline" className="mt-6">
+                <LayoutDashboard size={18} />
+                <span className='hidden md:inline'>Dashboard</span>
+              </Button>
+            </a>
+
+            {/* for dashboard */}
+            <a
+              href={"/transaction/create"}
+              className='flex items-center gap-2'>
+              <Button className="mt-6">
+                <PenBoxIcon size={18} />
+                <span className='hidden md:inline'> Add Transaction</span>
+              </Button>
+            </a>
+          </SignedIn>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  rootBox: "mt-6",
+                  userButtonAvatarBox: "w-14 h-14",
+                  avatarBox: "w-14 h-14",
+                },
+              }}
+            />
+          </SignedIn>
+
+
+        </div>
+      </nav>
     </div>
   )
 }
