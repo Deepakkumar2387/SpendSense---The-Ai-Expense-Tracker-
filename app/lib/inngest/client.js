1f193bd7398164265ce0437f6ce7688ad36d6ffb
Deepak Kumar <dipanshugothwal176@gmail.com>
2025-09-24 23:32:42 +0530

    check

---
+++ app/lib/inngest/client.js
@@ -0,0 +1,12 @@
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ 
+    id: "SpendSense",
+    name: "SpendSense",
+    retryFunction: async(attempt) => ({
+        delay: Math.pow(2,attempt) * 1000,
+        maxAttempts: 2,
+    })
+
+ });
